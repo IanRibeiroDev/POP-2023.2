3c97555a0ba988526a196690c93bb78ddad5f9eb
Ian-Ribeiro <ian.ribeiro@academico.ifpb.edu.br>
2023-11-10 17:43:58 -0300

    Organizando arquivos em pastas

---
+++ exercicios_resolvidos/slides/c++_basico_I/er1.cpp
@@ -0,0 +1,25 @@
+// Stefan, bicampeão da NBA (Navegação Baseada em Arrays) pelo Paysandu decidiu ensinar arremessos ao seu camarada Govboy.
+// Então, nosso Loiron James vai digitar três linhas: o número de cestas de 3 pontos, de 2 pontos e 1 ponto.
+// Ajude Govboy a calcular a pontuação total e aprender que no basquete as cestas podem resultar em pontuações diferentes.
+
+// Exemplo de Entrada:
+// 2
+// 1
+// 4
+
+// Exemplo de Saída:
+// Total: 12
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int x, y, z, pontuacao;
+    cin >> x;
+    cin >> y;
+    cin >> z;
+    pontuacao = x * 3 + y * 2 + z;
+    cout << "Total: " << pontuacao << "\n";
+    return 0;
+}
---
+++ exercicios_resolvidos/slides/c++_basico_I/er2.cpp
@@ -0,0 +1,46 @@
+// O professor Leônidas estava entediado e decidiu fazer a seguinte brincadeira na sala de aula.
+// Ele aleatoriamente irá atribuir um coeficiente K (0 <= K <= 100) e de acordo com o coeficiente recebido o aluno é classificado das seguintes formas:
+//    - K de 0 a 30 é considerado filão iniciante;
+//    - K de 31 a 59 é considerado esforçado;
+//    - K de 60 a 100 é considerado filão esperto;
+//
+// Dada uma lista com coeficientes de 5 alunos descubra quantos não são filões espertos.
+
+// Exemplo de Entrada:
+// 2 39 99 22 13
+
+// Exemplo de Saída:
+// 4
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int k1, k2, k3, k4, k5, resultado;
+    cin >> k1 >> k2 >> k3 >> k4 >> k5;
+
+    if (k1 < 60) {
+        resultado++;
+    }
+
+    if (k2 < 60) {
+        resultado++;
+    }
+
+    if (k3 < 60) {
+        resultado++;
+    }
+
+    if (k4 < 60) {
+        resultado++;
+    }
+
+    if (k5 < 60) {
+        resultado++;
+    }
+
+    cout << resultado;
+    return 0;
+}
