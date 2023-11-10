f512937692f2cd2c7e8fef0a582ed29cfc2c600d
Ian-Ribeiro <ian.ribeiro@academico.ifpb.edu.br>
2023-11-10 20:51:30 -0300

    Melhorando comentários e organização dos arquivos

--- exercicios_resolvidos/slides/c++_basico_I/er1.cpp
+++ exercicios_resolvidos/slides/c++_basico_I/er1.cpp
@@ -1,14 +1,16 @@
-// Stefan, bicampeão da NBA (Navegação Baseada em Arrays) pelo Paysandu decidiu ensinar arremessos ao seu camarada Govboy.
-// Então, nosso Loiron James vai digitar três linhas: o número de cestas de 3 pontos, de 2 pontos e 1 ponto.
-// Ajude Govboy a calcular a pontuação total e aprender que no basquete as cestas podem resultar em pontuações diferentes.
+/*
+Stefan, bicampeão da NBA (Navegação Baseada em Arrays) pelo Paysandu decidiu ensinar arremessos ao seu camarada Govboy.
+Então, nosso Loiron James vai digitar três linhas: o número de cestas de 3 pontos, de 2 pontos e 1 ponto.
+Ajude Govboy a calcular a pontuação total e aprender que no basquete as cestas podem resultar em pontuações diferentes.
 
-// Exemplo de Entrada:
-// 2
-// 1
-// 4
+Exemplo de Entrada:
+2
+1
+4
 
-// Exemplo de Saída:
-// Total: 12
+Exemplo de Saída:
+Total: 12
+*/
 
 #include <bits/stdc++.h>
 
--- exercicios_resolvidos/slides/c++_basico_I/er2.cpp
+++ exercicios_resolvidos/slides/c++_basico_I/er2.cpp
@@ -1,16 +1,18 @@
-// O professor Leônidas estava entediado e decidiu fazer a seguinte brincadeira na sala de aula.
-// Ele aleatoriamente irá atribuir um coeficiente K (0 <= K <= 100) e de acordo com o coeficiente recebido o aluno é classificado das seguintes formas:
-//    - K de 0 a 30 é considerado filão iniciante;
-//    - K de 31 a 59 é considerado esforçado;
-//    - K de 60 a 100 é considerado filão esperto;
-//
-// Dada uma lista com coeficientes de 5 alunos descubra quantos não são filões espertos.
+/*
+O professor Leônidas estava entediado e decidiu fazer a seguinte brincadeira na sala de aula.
+Ele aleatoriamente irá atribuir um coeficiente K (0 <= K <= 100) e de acordo com o coeficiente recebido o aluno é classificado das seguintes formas:
+- K de 0 a 30 é considerado filão iniciante;
+- K de 31 a 59 é considerado esforçado;
+- K de 60 a 100 é considerado filão esperto;
 
-// Exemplo de Entrada:
-// 2 39 99 22 13
+Dada uma lista com coeficientes de 5 alunos descubra quantos não são filões espertos.
 
-// Exemplo de Saída:
-// 4
+Exemplo de Entrada:
+2 39 99 22 13
+
+Exemplo de Saída:
+4
+*/
 
 #include <bits/stdc++.h>
 
